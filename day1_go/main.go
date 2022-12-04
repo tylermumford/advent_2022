230cb9fa6e018caa4828d560dd6fd35f357a418e
Tyler Mumford <tyler.mumford@icloud.com>
2022-12-04 00:26:43 -0700

    Solve day one part two

--- day1_go/main.go
+++ day1_go/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	fmt.Println("Starting...")
 
-	file, err := ioutil.ReadFile("sampleInput")
+	file, err := ioutil.ReadFile("input")
 	if err != nil {
 		panic(err)
 	}
@@ -18,17 +18,24 @@ func main() {
 	input := string(file)
 
 	answer := calculateAnswer(input)
-	fmt.Println("Answer: The elf with the most calories has", answer, "calories.")
+	fmt.Println("Part 1: The elf with the most calories has", answer.part1, "calories.")
+	fmt.Println("Part 2: The sum of the three highest-calorie elves is", answer.part2)
 }
 
-func calculateAnswer(input string) int {
-	answer := 0
+type answer struct {
+	part1 int
+	part2 int
+}
 
+func calculateAnswer(input string) answer {
 	elves := convertIntoElves(input)
 
 	fmt.Println("Loaded", len(elves), "elves")
 
-	answer = findHighestCalorieTotal(elves)
+	answer := answer{}
+
+	answer.part1 = findHighestCalorieTotal(elves)
+	answer.part2 = findSumOfThreeHighestTotals(elves)
 
 	return answer
 }
@@ -78,4 +85,27 @@ func findHighestCalorieTotal(elves []elf) int {
 	}
 
 	return highest
+}
+
+func findSumOfThreeHighestTotals(elves []elf) int {
+	var first, second, third int
+
+	for _, elf := range elves {
+		total := elf.calorieTotal
+
+		if total >= first {
+			third = second
+			second = first
+			first = total
+		} else if total >= second {
+			third = second
+			second = total
+		} else if total > third {
+			third = total
+		}
+	}
+
+	fmt.Println("First:", first, "Second:", second, "Third:", third)
+
+	return first + second + third
 }
