820f168ccef44832fa8f46275d135b10192c8932
Tyler Mumford <tyler.mumford@icloud.com>
2022-12-05 23:55:10 -0700

    Tried to solve day 4 part 2, but got the wrong answer

--- day4_go/main.go
+++ day4_go/main.go
@@ -11,11 +11,13 @@ func main() {
 
 	fmt.Println("Sample input:")
 	printFullOverlapCount(inputs.sample)
+	printOverlapCount(inputs.sample)
 
 	fmt.Println()
 
 	fmt.Println("Main input:")
 	printFullOverlapCount(inputs.input)
+	printOverlapCount(inputs.input)
 }
 
 func printFullOverlapCount(pairs []pair) {
@@ -34,10 +36,33 @@ func (p pair) hasFullOverlap() bool {
 	return p.first.fullyContains(p.second) || p.second.fullyContains(p.first)
 }
 
+func (p pair) hasAnyOverlap() bool {
+	return p.first.overlapsWith(p.second)
+}
+
 func (s span) fullyContains(other span) bool {
 	if other.from < s.from || other.to > s.to {
 		return false
 	}
 
 	return true
+}
+
+func (s span) overlapsWith(other span) bool {
+	hasBottomOverlap := (s.from <= other.from) && (other.from <= s.to)
+	hasTopOverlap := (s.from <= other.to) && (other.to <= s.to)
+
+	return hasBottomOverlap || hasTopOverlap
+}
+
+func printOverlapCount(pairs []pair) {
+	count := 0
+
+	for _, p := range pairs {
+		if p.hasAnyOverlap() {
+			count++
+		}
+	}
+
+	fmt.Println(count, "pairs with ANY overlap")
 }
