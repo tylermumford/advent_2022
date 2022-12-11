d8e7b722ec39ac08a51326676c19270fb124f251
Tyler Mumford <tyler.mumford@icloud.com>
2022-12-11 00:13:17 -0700

    Solve day 10 part 1!

--- day10_cs/Day10/Program.cs
+++ day10_cs/Day10/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day10;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code, Day 10");
+
+Console.WriteLine("Part 1:");
+Console.WriteLine($"Sum of indicated signal strengths: {answerPart1()}");
+
+int answerPart1()
+{
+	var input = Inputs.Main.ParseIntoInstructions();
+	var cpu = new Cpu();
+	var history = cpu.Execute(input);
+
+	var keySteps = new List<int>
+	{
+		20, 60, 100, 140, 180, 220,
+	};
+
+	return keySteps
+		.Select(step => history.SignalStrengthDuringStep(step))
+		.Sum();
+}
