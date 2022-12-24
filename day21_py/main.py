c312d9de1eb9558ec9bdfa34c636903016e1b4eb
Tyler Mumford <tyler.mumford@icloud.com>
2022-12-24 16:01:06 -0700

    Start working on day 21

---
+++ day21_py/main.py
@@ -0,0 +1,11 @@
+def main(filename):
+    with open(filename, encoding="utf-8") as data:
+        pass
+
+    print(filename)
+
+if __name__ == '__main__':
+    main('sample-input.txt')
+    print('\n')
+    main('main-input.txt')
+
