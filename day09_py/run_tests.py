c1bf794cbda9820c1e913618c58d1c2fd90a4870
Tyler Mumford <tyler.mumford@icloud.com>
2022-12-25 01:52:04 -0700

    Support moving to the right

--- day09_py/run_tests.py
+++ day09_py/run_tests.py
@@ -1,5 +1,10 @@
 import unittest
-from world import World
+from world import *
+from direction import Direction
+from command import Command
+import logging
+
+logging.basicConfig(level=logging.WARNING)
 
 class RunTests(unittest.TestCase):
     def test_NoMovement(self):
@@ -9,3 +14,23 @@ class RunTests(unittest.TestCase):
 
         # The tail starts somewhere, and that counts
         self.assertEqual(w.distinctTailPositions, 1)
+
+    def test_RightTwo(self):
+        w = World(visible=True)
+
+        w.run([Command('R', 2)])
+
+        self.assertEqual(w.distinctTailPositions, 2)
+
+    def test_mv(self):
+        self.assertEqual(mv((0, 0), Direction.RIGHT), (1, 0))
+
+    def test_dist(self):
+        d1 = dist((0,0), (1,0))
+        d2 = dist((0,0), (2,0))
+
+        self.assertEqual(d1, 1)
+        self.assertEqual(d2, 2)
+
+if __name__ == '__main__':
+    unittest.main()
--- day09_py/world.py
+++ day09_py/world.py
@@ -1,12 +1,71 @@
+import logging
+from direction import Direction
+
 class World():
     def __init__(self, visible=False):
-        self.distinctTailPositions = 1
         self.visible = visible
 
-    def run(self, commands):
-        self._p(commands)
+        self.head = (0,0)
+        self.tail = (0,0)
 
+        self.visited = set((0,0))
+
+    @property
+    def distinctTailPositions(self):
+        return len(self.visited)
+
+    def run(self, commands):
+        logging.debug(f"Running {len(commands)} commands")
+
+        for c in commands:
+            logging.debug(f"Running command {c}")
+
+            self.runCommand(c)
+
+    def runCommand(self, c):
+        for i in range(0, c.steps):
+            self.step(c.direction)
+
+    def step(self, direction):
+        logging.debug(f"Moving one to the {direction}")
+
+        self.head = mv(self.head, direction)
+        self.stepTail()
+
+    def stepTail(self):
+        gap = dist(self.head, self.tail)
+        logging.debug(f"gap is {gap}")
+
+        if gap > 1:
+            self.catchUp()
+
+    def catchUp(self):
+        dx = distx(self.tail, self.head)
+        x = self.tail[0] + (1 if dx > 1 else 0)
+        self.tail = (x, self.tail[1])
+
+        # TODO: dy, y
+        self.visited.add(self.tail)
 
     def _p(self, args):
         if self.visible:
             print(args)
+
+def mv(pair, direction):
+    'Move the coordinates in `pair` one step in the `direction` given.'
+    if direction == Direction.RIGHT:
+        return pair[0] + 1, pair[1]
+
+    raise f"{direction} not supported by mv function"
+    # TODO: More directions
+
+def dist(pair1, pair2):
+    horiz = abs(pair1[0] - pair2[0])
+    vert = abs(pair1[1] - pair2[1])
+    return max(horiz, vert)
+
+def distx(pair1, pair2):
+    return pair2[0] - pair1[0]
+
+def disty(pair1, pair2):
+    return pair2[1] - pair1[1]
