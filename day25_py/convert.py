af8bae004d22859b8e3e05696f03be998bbb46b1
Tyler Mumford <tyler.mumford@icloud.com>
2022-12-26 00:00:50 -0700

    Implement some toSnafu

--- day25_py/convert.py
+++ day25_py/convert.py
@@ -1,5 +1,6 @@
 # Written to be run by pytest
 
+import math
 import pytest
 
 class TestConvert:
@@ -20,8 +21,47 @@ class TestConvert:
         d = toDecimal(s)
         assert expect == d
 
+    @pytest.mark.parametrize(
+            "d,expect",
+            [
+                (0, "0"),
+                (1, "1"),
+                (2, "2"),
+                (10, "20"),
+                (25, "100"),
+            ])
+           # [("2=-01", 976),
+           #  ("1-0---0", 12345),
+           #  ("1121-1110-1=0", 314159265)])
+    def testToSnafu(self, d, expect):
+        s = toSnafu(d)
+        assert expect == s
+
 def toSnafu(n):
     "Converts n (decimal) to a SNAFU string."
+    if n == 0:
+        return "0"
+
+    tokens = []
+    exponent = math.floor(math.log(n, 5))
+    descending = range(exponent, -1, -1)
+    divisors = [5 ** x for x in descending]
+
+    for divisor in divisors:
+        count, remain = divmod(n, divisor)
+        print(f"{n} // {divisor} -> {count}, {remain}")
+        if count > 0:
+            tokens.append(str(count))
+        else:
+            tokens.append(str(remain))
+            #break
+
+        n = remain
+
+        print(tokens)
+
+    return "".join(tokens)
+
 
 def toDecimal(snafu):
     "Converts a SNAFU string to a number."
