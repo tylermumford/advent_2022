43296f0c763f4ede7428b26fcf5b6d291eb0a6e8
Tyler Mumford <tyler.mumford@icloud.com>
2022-12-28 23:48:35 -0700

    Refactor

--- day11_py/main.py
+++ day11_py/main.py
@@ -5,12 +5,10 @@ from setup_sample import Monkeys as SampleMonkeys
 
 def main():
     performRounds(20, SampleMonkeys)
+    showMonkeyBusiness(SampleMonkeys)
 
-    business = monkeyBusiness(SampleMonkeys)
-    print("Monkey business:", business)
-
-    assert business in [10605, 101436, 2713310158], \
-        f"Monkey business was an unexpected {business}"
+    #performRounds(10000, SampleMonkeys)
+    #showMonkeyBusiness(SampleMonkeys)
 
 def performRounds(count, monkeyGroup):
     for i in range(0, count):
@@ -23,15 +21,19 @@ def allMonkeysTakeATurn(monkeyGroup):
 
     pp(monkeyGroup)
 
-def monkeyBusiness(monkeyGroup):
-    allMonkeys = [m for m in monkeyGroup]
-    byActivity = sorted(
+def showMonkeyBusiness(monkeyGroup):
+    byInspections = sorted(
             monkeyGroup.values(),
             key=lambda m: m.inspectionCount,
             reverse=True)
-    topTwo = byActivity[0:2]
+    topTwo = byInspections[0:2]
     inspections = [m.inspectionCount for m in topTwo]
-    return inspections[0] * inspections[1]
+    business = inspections[0] * inspections[1]
+
+    print("Monkey business:", business)
+
+    assert business in [10605, 101436, 2713310158], \
+        f"Monkey business was an unexpected {business}"
 
 if __name__ == "__main__":
     main()
