67feeb8e0d363e3fe4d50cb5f8b7c2eb6a47cb95
Tyler Mumford <tyler.mumford@icloud.com>
2023-02-20 00:59:42 -0700

    Solve day 3 part 1 with Rust

--- day03_rust/src/main.rs
+++ day03_rust/src/main.rs
@@ -3,9 +3,33 @@ use std::fs;
 fn main() {
     println!("Hello, world!");
     let sample = get_input("sampleInput");
+    let mut priority_sum = 0;
+
     for line in sample {
-        println!("\t{line}")
+        println!("\t{line}");
+        let doubled = find_doubled_item(&line);
+        let priority = letter_to_priority(&doubled);
+        println!("\t  {doubled} ({priority})");
+
+        priority_sum += priority;
     }
+
+    println!("priority sum: {priority_sum}");
+
+    // Same as above, but with less output
+    println!();
+
+    let sample = get_input("input");
+    let mut priority_sum = 0;
+
+    for line in sample {
+        let doubled = find_doubled_item(&line);
+        let priority = letter_to_priority(&doubled);
+
+        priority_sum += priority;
+    }
+
+    println!("priority sum: {priority_sum}")
 }
 
 fn get_input(filename: &str) -> Vec<String> {
@@ -19,6 +43,30 @@ fn get_input(filename: &str) -> Vec<String> {
         result.push(line.to_string())
     }
     result
+}
+
+fn find_doubled_item(rucksack: &String) -> char {
+    let len = rucksack.len() / 2;
+    let first = &rucksack[0..len];
+    let second = &rucksack[len..];
+
+    for f in first.chars() {
+        for s in second.chars() {
+            if s == f {
+                return s
+            }
+        }
+    }
+
+    unimplemented!("not in problem bounds: {rucksack}")
+}
+
+fn letter_to_priority(letter: &char) -> u32 {
+    match letter {
+        'a'..='z' => (*letter as u32) - ('a' as u32) + 1,
+        'A'..='Z' => (*letter as u32) - ('A' as u32) + 27,
+        _ => unimplemented!("no priority for {letter}"),
+    }
 }
 
 #[cfg(test)]
@@ -42,5 +90,40 @@ mod tests {
         let input = get_input("sampleInput");
 
         assert_eq!(input.len(), 6);
+    }
+
+    #[test]
+    fn find_doubled_item_works() {
+        let result = find_doubled_item(&"abcdec".to_string());
+        assert_eq!(result, 'c');
+    }
+
+    #[test]
+    fn letter_to_priority_works() {
+        let scores = Vec::from_iter(1..=52);
+        let letters = help_letters();
+
+        for i in 0..letters.len() {
+            let s = letter_to_priority(&letters[i]);
+            assert_eq!(scores[i], s);
+        }
+    }
+
+    fn help_letters() -> Vec<char> {
+        let mut v = Vec::new();
+        let mut next = 'a' as u8;
+
+        for incr in 0..=25 {
+            let push = (next + incr) as char;
+            v.push(push);
+        }
+
+        next = 'A' as u8;
+        for incr in 0..=25 {
+            let push = (next + incr) as char;
+            v.push(push);
+        }
+
+        v
     }
 }
